e5c88549ff3d226ad0d7388ed03c1c67a85b38b5
root <x-cyber@windowslive.com>
2021-07-05 10:47:04 +0700

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,29 @@
+#!/bin/bash
+
 # gcc -o test main.cpp -I./lua52/include/ -L./lua52/ ./lua52/liblua.a
 # gcc -o test main.cpp -I./lua52/include/ -L./lua52/ -l:./lua52/liblua52.a
 # gcc -o test main.cpp -I./lua54/include/ -L./lua54/ -l./lua54/liblua.a
-gcc -o test main.cpp -lstdc++ -std=c++11 -I./lua54/include/ -L./lua54/ -llua -lm
 # g++ -o test main.cpp -lstdc++ -std=c++11 -I./lua54/include/ -L./lua54/ -llua -lm
+
+if [ -z "$1" ]
+then
+	echo "please enter platform name";
+	exit;
+fi
+
+if [[ "$1" == 'macosx' ]];
+then
+	# work on macosx
+	gcc -o test main.cpp -lstdc++ -std=c++11 -I./lua54/include/ -L./lua54/ -llua -lm
+elif [[ "$1" == 'linux' ]];
+then
+	# work on linux
+	gcc -o test main.cpp -lstdc++ -std=c++11 -I./lua54/include/ -L./lua54/ -llua -lm -Wl,-E -ldl -lreadline 
+elif [[ "$1" == 'windows' ]];
+then
+	echo 'windows soon';
+else
+	echo "unknow platform support";
+fi
+
+
--- run.sh
+++ run.sh
@@ -1,2 +1,12 @@
-./build.sh
+#!/bin/bash
+
+
+if [ -z "$1" ]
+then
+	echo "please enter platform name";
+	exit;
+fi
+
+./build.sh $1
 ./test
+
